8695386cab0fe14c208208af62fbce5cbcb55575
Asif Reza Chowdhury <62113977+asifrezac@users.noreply.github.com>
2021-05-06 17:34:19 +0600

    class 1

---
+++ AdmissionModularized/AdmissionModularized/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+namespace AdmissionModularized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter your GPA: ");
+            double GPA = Convert.ToDouble(ReadLine());
+
+            Write("Enter your test score: ");
+            int testScore = Convert.ToInt32(ReadLine());
+
+            Write("With {0} GPA and {1} Test Score, the admission status is {2}.", GPA, testScore, AdmissionStatus(GPA, testScore));
+        }
+        public static String AdmissionStatus(double GPA, int testScore)
+        {
+            const int TSL1 = 60, TSL2 = 80;
+            const double GPACR = 3.0;
+            string status;
+
+            if (GPA >= GPACR && testScore >= TSL1)
+                status = "Accept";
+            else if (GPA < GPACR && testScore >= TSL2)
+                status = "Accept";
+            else
+                status = "Reject";
+
+            return status;
+        }
+    }
+}
---
+++ PaintingEstimate/PaintingEstimate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+namespace PaintingEstimate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Input the length of the room: ");
+            int length = Convert.ToInt32(ReadLine());
+
+            Write("Input the width of the room: ");
+            int width = Convert.ToInt32(ReadLine());
+
+            Write("The estimate painting cost for a room with {0} feet length and {1} feet width is {2}.", length, width, PaintCost(length, width).ToString("C"));
+        }
+
+        public static Double PaintCost(int length, int width)
+        {
+            const int height = 9;
+            const double paintRate = 6.00;
+            int area = 2 * ((length * height) + (width * height));
+
+            double cost = paintRate * Convert.ToDouble(area);
+
+            return cost;
+        }
+    }
+}
