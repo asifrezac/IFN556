377549c36b489d0ab8059e8cfbc647494c791610
Asif Reza Chowdhury <62113977+asifrezac@users.noreply.github.com>
2021-05-13 16:10:42 +0600

    week 10

---
+++ MovieTime/MovieTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+namespace MovieTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieDuration("Matrix", 113);
+            MovieDuration("The Greenbook");
+            MovieDuration();
+        }
+
+        static void MovieDuration(string name = "300", int duration = 90)
+        {
+            WriteLine("The {0} movie is {1} minutes long.", name, duration);
+        }
+    }
+}
