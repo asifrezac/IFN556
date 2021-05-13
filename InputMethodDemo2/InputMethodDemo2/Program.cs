8b829031999f945a7012b1439fb9c6da065fe6b6
Asif Reza Chowdhury <62113977+asifrezac@users.noreply.github.com>
2021-05-13 16:11:41 +0600

    week 10

---
+++ InputMethodDemo2/InputMethodDemo2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+namespace InputMethodDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second;
+            InputMethod(out first, out second);
+            WriteLine("The first value is {0} and the second value is {1}.", first, second);
+
+        }
+
+        private static void InputMethod(out int one, out int two)
+        {
+            one = DataEntry("first");
+            two = DataEntry("second");
+        }
+
+        private static int DataEntry(string whichOne)
+        {
+            int one;
+            string S1;
+            WriteLine("Please enter {0} value.", whichOne);
+            S1 = ReadLine();
+            one = Convert.ToInt32(S1);
+            return one;
+        }
+    }
+}
